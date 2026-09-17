8e40d2dcaa16eb956920f80b33e27a372d1843c4
Beymar <yefreybeimar@2005gmail.com>
2026-09-16 22:27:02 -0500

    fase 3: maquina de aplicacion con script de arranque

--- main.tf
+++ main.tf
@@ -22,3 +22,26 @@ resource "google_compute_subnetwork" "publica" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
+# main.tf (añadir)
+resource "google_compute_instance" "app" {
+  name         = "${var.prefijo}-app"
+  machine_type = var.tipo_maquina
+  zone         = var.zona
+  tags         = ["servidor-web"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    # la máquina debe quedar en tu subred, no en la default
+    subnetwork = var.subred
+    # un bloque vacío aquí otorga una IP pública efímera
+    access_config {}
+  }
+
+  metadata_startup_script = file("arranque.sh")
+}
--- variables.tf
+++ variables.tf
@@ -31,3 +31,8 @@ variable "cidr_privada" {
     type = string
     default = "10.10.2.0/24"
 }
+
+variable "subred" {
+    type = string
+    default = "https://www.googleapis.com/compute/v1/projects/project-3111890e-6ba4-4e0e-95f/regions/us-central1/subnetworks/villamizar-sub-publica"
+}
